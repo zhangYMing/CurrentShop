da1d93f769f73fa6b0bbb866dd4308ad528203df
張一鳴 <@DESKTOP-BLJKE7V>
2017-09-25 15:43:12 +0900

    注册画面概要

---
+++ MyBallShop/Controllers/CreateAcountMsgController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBallShop.Controllers
+{
+    public class CreateAcountMsgController : Controller
+    {
+        // GET: CreateAcountMsg
+        public ActionResult CreateAcount()
+        {
+            return View();
+        }
+    }
+}
