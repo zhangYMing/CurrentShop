a73e1eb249b5196fed566e53cc5d8997e6dac80b
張一鳴 <@DESKTOP-BLJKE7V>
2017-12-04 17:21:36 +0900

    新规做成

---
+++ MyCurrentShop/MyCurrentShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyCurrentShop.Startup))]
+namespace MyCurrentShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
